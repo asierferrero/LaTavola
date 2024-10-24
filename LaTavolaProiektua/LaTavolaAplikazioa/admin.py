7372376303746159d1d2791379fd2d4a9dd598b6
asierferrero <asierferrero09@gmail.com>
2024-10-24 11:39:07 +0200

    Update admin.py

--- LaTavolaProiektua/LaTavolaAplikazioa/admin.py
+++ LaTavolaProiektua/LaTavolaAplikazioa/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Erabiltzailea)
+admin.site.register(Hornitzailea)
+admin.site.register(Langilea)
+admin.site.register(Alergeno)
+admin.site.register(Produktua)
+admin.site.register(AlergenoProduktua)
+admin.site.register(Eskaria)
+admin.site.register(Iritzia)
