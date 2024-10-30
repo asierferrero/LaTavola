77fcd89ced08f6dc88cccc70c721f77d049376e1
asierferrero <asierferrero09@gmail.com>
2024-10-30 12:20:57 +0100

    fix (#17): sites kenduta

--- LaTavolaProiektua/LaTavolaAplikazioa/admin.py
+++ LaTavolaProiektua/LaTavolaAplikazioa/admin.py
@@ -3,10 +3,8 @@ from .models import *
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-admin.site.register(Hornitzailea)
 admin.site.register(Langilea)
 admin.site.register(Alergeno)
 admin.site.register(Produktua)
-admin.site.register(AlergenoProduktua)
 admin.site.register(Eskaria)
 admin.site.register(Iritzia)
