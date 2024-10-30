da01842bbd1bd03c124a522a33ac8deed30ab27b
Aritz <arruabarrena.aritz@uni.eus>
2024-10-30 12:34:22 +0100

    feat(#33): ProduktuakGehitu sql

---
+++ ProduktuakGehitu.sql
@@ -0,0 +1,11 @@
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua (izena, deskripzioa, img, prezioa, stock) VALUES
+('Margarita Pizza', 'Italiar pizza klasikoa tomate, mozzarella eta albakarekin.', '/static/img/pizza.png', 9.99, 10),
+('Carbonara Pasta', 'Pasta goxoa arrautza, gazta eta hirugihar saltsa krematsuarekin', '/static/img/pasta.png', 9.99, 10),
+('Quattro Formaggi Pizza', 'Italiar pizza lau gaztarekin: mozzarella, gorgonzola, parmigiano eta ricotta.', '/static/img/pasta.png', 9.99, 10),
+('Hawaiian Pizza', 'Pizza exotikoa, urdaiazpiko eta anana xerrekin.', '/static/img/pasta.png', 9.99, 10),
+('Vegetarian Pizza', 'Barazkiz betetako pizza: tomate, piper, perretxiko, eta artoarekin.', '/static/img/pasta.png', 9.99, 10),
+('BBQ Chicken Pizza', 'Oilasko pizza saltsa barbekuarekin, mozzarella eta tipularekin.', '/static/img/pasta.png', 9.99, 10),
+('Diavola Pizza', 'Pikantea, txorizo eta piper beroarekin prestatua.', '/static/img/pasta.png', 9.99, 10),
+('Capricciosa Pizza', 'Pizza oparoa: perretxiko, urdaiazpiko, oliba eta artichokekin.', '/static/img/pasta.png', 9.99, 10),
+('Marinara Pizza', 'Tomate, baratxuri eta oreganoa besterik ez duen pizza klasikoa.', '/static/img/pasta.png', 9.99, 10),
+('Prosciutto e Funghi Pizza', 'Prosciutto urdaiazpiko eta perretxikoekin egindako pizza gozoa.', '/static/img/pasta.png', 9.99, 10);
