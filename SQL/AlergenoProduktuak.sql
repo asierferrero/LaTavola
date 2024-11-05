bd9fc8db87550d85306e0519c1b9de4b283a9f42
Julen Peláez <pelaez.julen@uni.eus>
2024-11-05 09:18:33 +0100

    Feat (#45): Sql-ak gehitu

---
+++ SQL/AlergenoProduktuak.sql
@@ -0,0 +1,68 @@
+-- Bruschetta al Pomodoro (podría contener gluten debido al pan)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (1, 3);
+
+-- Carpaccio di Manzo (contiene lácteos por el parmesano)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (2, 4);
+
+-- Arancini di Riso (puede contener gluten por la fritura)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (3, 3);
+
+-- Insalata Caprese (contiene lácteos por la mozzarella)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (4, 4);
+
+-- Crostini di Fegato (contiene gluten y lácteos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (5, 3);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (5, 4);
+
+-- Tagliatelle al Tartufo (contiene gluten y lácteos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (6, 3);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (6, 4);
+
+-- Ravioli di Ricotta e Spinaci (contiene gluten y lácteos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (7, 3);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (7, 4);
+
+-- Pappardelle al Cinghiale (contiene gluten)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (8, 3);
+
+-- Spaghetti alle Vongole (puede contener gluten por la pasta y mariscos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (9, 3);
+
+-- Lasagna Tradizionale (contiene gluten y lácteos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (10, 3);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (10, 4);
+
+-- Osso Buco alla Milanese (contiene gluten y lácteos por la gremolata y el risotto)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (11, 3);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (11, 4);
+
+-- Saltimbocca alla Romana (contiene lácteos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (12, 4);
+
+-- Filetto di Manzo al Barolo (puede contener lácteos por el puré de trufa)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (14, 4);
+
+-- Risotto ai Funghi (puede contener lácteos por el uso de queso en la preparación)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (18, 4);
+
+-- Polenta alla Griglia (puede contener lácteos según la receta)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (20, 4);
+
+-- Tiramisu Classico (contiene lácteos y huevos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (21, 2);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (21, 4);
+
+-- Panna Cotta al Caramello (contiene lácteos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (22, 4);
+
+-- Profiteroles al Cioccolato (contiene gluten, lácteos y huevos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (23, 3);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (23, 4);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (23, 2);
+
+-- Crostata di Frutta (contiene gluten y lácteos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (24, 3);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (24, 4);
+
+-- Gelato Artigianale (contiene lácteos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (25, 4);
