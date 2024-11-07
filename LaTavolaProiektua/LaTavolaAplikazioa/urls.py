e8579d110f8d5f66bdc7a42a9eeb3d0626a2fc8b
Aritz <arruabarrena.aritz@uni.eus>
2024-11-07 10:17:55 +0100

    feat(#41):Admin horrialdeara joateko botoia

--- LaTavolaProiektua/LaTavolaAplikazioa/urls.py
+++ LaTavolaProiektua/LaTavolaAplikazioa/urls.py
@@ -5,11 +5,13 @@ urlpatterns = [
     path('', views.main, name='home'),
     
     path('admin-home/', views.admin_home_view , name='admin_home'),
-    path('admin-home/bezeroak-list/', views.admin_bezeroak_list , name='bezeroak-list'),
+    
     path('admin-home/produktuak-list/', views.admin_produktuak_list , name='produktuak-list'),
     path('admin-home/produktuak-new/', views.produktua_new , name='produktuak-new'),
     path('produktuak/delete/<int:id>/', views.produktuak_delete, name='produktuak-delete'),
     path('produktuak/edit/<int:id>/', views.produktuak_edit, name='produktuak-edit'),
+    
+    path('admin-home/bezeroak-list/', views.admin_bezeroak_list , name='bezeroak-list'),
     path('bezeroak/delete/<int:id>/', views.bezeroak_delete, name='bezeroak-delete'),
     path('bezeroak/edit/<int:id>/', views.bezero_edit, name='bezero-edit'),
     
