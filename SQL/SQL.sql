d2d752be24d5c1d990bcc1c030d0dbc3815c32c2
asierferrero <asierferrero09@gmail.com>
2024-11-08 12:55:02 +0100

    Create SQL.sql

---
+++ SQL/SQL.sql
@@ -0,0 +1,122 @@
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua (izena, deskripzioa, img, prezioa, stock, mota, adin_nagusikoa) VALUES
+('Bruschetta al Pomodoro', 'Tomate freskoz egindako ogi tostatua, albahaka hosto aromatikoekin eta oliba olio birjina esklusibo batekin.', '/static/img/bruschetta.png', 8.50, 15, 'Hasierakoa', FALSE),
+('Carpaccio di Manzo', 'Behi haragi finak, trufa olio sotilarekin, errukula gaztarekin eta parmesano bikainekin biribilduak.', '/static/img/carpaccio.png', 12.00, 10, 'Hasierakoa', FALSE),
+('Arancini di Riso', 'Errizoma krematsuz betetako bola kurruskariak, tomate murrizketa aberats batekin zerbitzatuta.', '/static/img/arancini.png', 10.50, 20, 'Hasierakoa', FALSE),
+('Insalata Caprese', 'Mozzarella freskoa eta tomate heirloom koloretsuak, albahaka freskoarekin eta balsamiko murrizketa leunekin.', '/static/img/caprese.png', 9.00, 15, 'Hasierakoa', FALSE),
+('Crostini di Fegato', 'Oilasko gibelez egindako kremaz betetako crostiniak, tipula karamelizatuarekin eta pistatxo birrinduekin apainduak.', '/static/img/crostini.png', 11.00, 10, 'Hasierakoa', FALSE),
+('Tagliatelle al Tartufo', 'Etxeko tagliatelle finak, trufa beltz apartarekin eta parmesano esentzialaren kremaz.', '/static/img/tagliatelle.png', 16.00, 12, 'Lehena', FALSE),
+('Ravioli di Ricotta e Spinaci', 'Ricotta eta espinaka goxoarekin betetako ravioli freskoak, gurin urtuzko eta salbia xerra fin batzuekin.', '/static/img/ravioli.png', 14.50, 12, 'Lehena', FALSE),
+('Pappardelle al Cinghiale', 'Pappardelle italiarrak, jabali haragi usaintsuz eta belar aromatikoen dotoretasunez zerbitzatuak.', '/static/img/pappardelle.png', 17.00, 8, 'Lehena', FALSE),
+('Spaghetti alle Vongole', 'Espagueti finak, txangurro freskoekin, baratxuri eta perrexil ukitu arinekin.', '/static/img/spaghetti_vongole.png', 15.00, 10, 'Lehena', FALSE),
+('Lasagna Tradizionale', 'Lasagna tradizionala, haragi ukitu sofistikatuarekin eta besamel krematsuz.', '/static/img/lasagna.png', 13.50, 10, 'Lehena', FALSE),
+('Osso Buco alla Milanese', 'Behiki txuleta delikatua, gremolata freskagarri eta risotto bikainarekin.', '/static/img/osso_buco.png', 22.00, 8, 'Bigarrena', FALSE),
+('Saltimbocca alla Romana', 'Serrano urdaiazpikoarekin bildutako oilasko bularra, salbia aromatikoa eta ardo zuri apainarekin.', '/static/img/saltimbocca.png', 18.00, 10, 'Bigarrena', TRUE),
+('Branzino al Sale', 'Gatza labean prestatutako lubina, barazki erreak eta olio esentziala lagun.', '/static/img/branzino.png', 25.00, 5, 'Bigarrena', FALSE),
+('Filetto di Manzo al Barolo', 'Behi filetea Barolo ardoan prestatua, trufa pure krematsu eta biribilduarekin.', '/static/img/filetto.png', 28.00, 6, 'Bigarrena', TRUE),
+('Pollo al Limone', 'Limoi usaintsuz eta belar gozoekin marinatutako oilasko bular fin-fina.', '/static/img/pollo.png', 17.00, 10, 'Bigarrena', FALSE),
+('Patate al Rosmarino', 'Erramu eta baratxuriz bustitako patata kurruskariak, zapore osoa bermatuz.', '/static/img/patate_rosmarino.png', 5.50, 20, 'Gehigarria', FALSE),
+('Verdure Grigliate', 'Denboraldiko barazkiak, parrillan erreak eta oliba olio birjina bikainez apainduak.', '/static/img/verdure.png', 6.00, 15, 'Gehigarria', FALSE),
+('Risotto ai Funghi', 'Perretxiko krematsuz egindako risotto leun eta aberatsa.', '/static/img/risotto.png', 14.00, 10, 'Gehigarria', FALSE),
+('Fagiolini al Vapore', 'Gatz ukituz eta zaporetsu zerbitzatutako babak.', '/static/img/fagiolini.png', 6.50, 20, 'Gehigarria', FALSE),
+('Polenta alla Griglia', 'Polenta labean prestatua, tomate saltsa gozo batekin zerbitzatua.', '/static/img/polenta.png', 8.00, 15, 'Gehigarria', FALSE),
+('Tiramisu Classico', 'Kafe eta mascarpone kremaren perfekzioa, kakao ukituarekin biribiltzeko.', '/static/img/tiramisu.png', 7.50, 12, 'Postrea', FALSE),
+('Panna Cotta al Caramello', 'Karamelo murrizketarekin zerbitzatutako panna cotta krematsua.', '/static/img/panna_cotta.png', 8.00, 10, 'Postrea', FALSE),
+('Profiteroles al Cioccolato', 'Esne kremaz betetako profiterolak, txokolate bero eta leunez bustiak.', '/static/img/profiteroles.png', 9.50, 12, 'Postrea', FALSE),
+('Crostata di Frutta', 'Fruitu freskoz egindako tarta, urtaroaren osagai goxoekin.', '/static/img/crostata.png', 6.50, 10, 'Postrea', FALSE),
+('Gelato Artigianale', 'Gelato artisau berezia, vanilla, txokolate eta pistatxozko zapore dotoreetan.', '/static/img/gelato.png', 4.50, 20, 'Postrea', FALSE),
+('Caffè Espresso', 'Italiar espresso autentikoa, zapore sakon eta biziarekin.', '/static/img/espresso.png', 2.50, 30, 'Kafea', FALSE),
+('Caffè Corretto', 'Espresso italiarra likore fin batekin osatua.', '/static/img/caffe_corretto.png', 3.00, 20, 'Kafea', TRUE),
+('Amaro', 'Digestibo italiar sofistikatuak, freskagarri zerbitzatuak.', '/static/img/amaro.png', 4.00, 25, 'Kafea', TRUE),
+('Grappa', 'Grappa tradizionala, italiar esentzia sakonarekin.', '/static/img/grappa.png', 5.00, 25, 'Kafea', TRUE),
+('Limoncello', 'Limoizko likore freskoa, amaiera perfektua.', '/static/img/limoncello.png', 4.50, 20, 'Kafea', TRUE);
+
+INSERT INTO latavolaprueba.latavolaaplikazioa_alergeno (izena, img) VALUES
+('Soja','/static/img/soja.png'),
+('Arrautzak','/static/img/arrautzak.png'),
+('Gluten','/static/img/gluten.png'),
+('Esnekiak','/static/img/esneketak.png');
+
+INSERT INTO latavolaprueba.latavolaaplikazioa_langilea (izena, abizena, postua, kontu_zenbakia) VALUES
+('Asier', 'Ferrero', 'Chef', 'ES678283940835262632'),
+('Aritz', 'Arruabarrena', 'Zerbitzaria', 'ES758629564732626329'),
+('Aiert', 'De La Torre', 'Kudeatzailea', 'ES458283940383262632'),
+('Julen', 'Pelaez', 'Banatzailea', 'ES548281128563262632'),
+('Gerson', 'Calo', 'Banatzailea', 'ES762847463838922341');
+
+-- Bruschetta al Pomodoro (podría contener gluten debido al pan)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (1, 3);
+
+-- Carpaccio di Manzo (contiene lácteos por el parmesano)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (2, 4);
+
+-- Arancini di Riso (puede contener gluten por la fritura)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (3, 3);
+
+-- Insalata Caprese (contiene lácteos por la mozzarella)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (4, 4);
+
+-- Crostini di Fegato (contiene gluten y lácteos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (5, 3);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (5, 4);
+
+-- Tagliatelle al Tartufo (contiene gluten y lácteos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (6, 3);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (6, 4);
+
+-- Ravioli di Ricotta e Spinaci (contiene gluten y lácteos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (7, 3);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (7, 4);
+
+-- Pappardelle al Cinghiale (contiene gluten)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (8, 3);
+
+-- Spaghetti alle Vongole (puede contener gluten por la pasta y mariscos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (9, 3);
+
+-- Lasagna Tradizionale (contiene gluten y lácteos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (10, 3);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (10, 4);
+
+-- Osso Buco alla Milanese (contiene gluten y lácteos por la gremolata y el risotto)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (11, 3);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (11, 4);
+
+-- Saltimbocca alla Romana (contiene lácteos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (12, 4);
+
+-- Filetto di Manzo al Barolo (puede contener lácteos por el puré de trufa)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (14, 4);
+
+-- Risotto ai Funghi (puede contener lácteos por el uso de queso en la preparación)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (18, 4);
+
+-- Polenta alla Griglia (puede contener lácteos según la receta)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (20, 4);
+
+-- Tiramisu Classico (contiene lácteos y huevos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (21, 2);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (21, 4);
+
+-- Panna Cotta al Caramello (contiene lácteos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (22, 4);
+
+-- Profiteroles al Cioccolato (contiene gluten, lácteos y huevos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (23, 3);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (23, 4);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (23, 2);
+
+-- Crostata di Frutta (contiene gluten y lácteos)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (24, 3);
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (24, 4);
+
+-- Gelato Artigianale (contiene lácteos y frutos secos, por el pistacho)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (25, 4);
+
+-- Caffè Espresso (puede contener lácteos si se sirve con leche)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (26, 4);
+
+-- Caffè Corretto (puede contener lácteos y frutos secos si se sirve con leche o ciertos licores)
+INSERT INTO latavolaprueba.latavolaaplikazioa_produktua_alergenoak (produktua_id, alergeno_id) VALUES (27, 4);
+
+
+
