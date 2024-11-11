e947c9e6bd3912ecb0a59229ae93a5df82a4347f
asierferrero <asierferrero09@gmail.com>
2024-11-11 10:08:41 +0100

    feat (#17): Menuaren estiloa hobetuta

--- LaTavolaProiektua/LaTavolaAplikazioa/static/js/menu.js
+++ LaTavolaProiektua/LaTavolaAplikazioa/static/js/menu.js
@@ -3,45 +3,57 @@ $(document).ready(function () {
     url: "/api/produktuak/",
     method: "GET",
     success: function (data) {
+      const ordena = ["Hasierakoa", "Lehena", "Bigarrena", "Gehigarria", "Postrea", "Kafea"];
+      data.sort((a, b) => ordena.indexOf(a.mota) - ordena.indexOf(b.mota));
+
+      let currentCategory = "";
       data.forEach(function (item, index) {
+        if (item.mota !== currentCategory) {
+          currentCategory = item.mota;
+          if (currentCategory === 'Kafea') {
+            $("#menu-items").append(`<div class="col-12 category-title ${currentCategory}"><h2>${currentCategory}k eta edatekoak</h2></div>`);
+          } else {
+            $("#menu-items").append(`<div class="col-12 category-title ${currentCategory}"><h2>${currentCategory}k</h2></div>`);
+          }
+        }
+
         let alergenoak = item.alergenoak.map(alergeno => `
-          <img src="${alergeno.img}" style="width: 40px; height: 40px; margin-right: 10px" title="${alergeno.izena}" />
-        `).join('');
+          <img src="${alergeno.img}" style="width: 30px; height: 30px; margin-right: 5px" title="${alergeno.izena}" />
+        `).join("");
 
         $("#menu-items").append(`
-          <div class="col-12 col-md-2">
-            <div class="menu-item card">
+          <div class="col-12 col-md-3 menu-item ${item.mota}">
+            <div class="card" data-bs-toggle="modal" data-bs-target="#productModal-${index}">
               <img src="${item.img}" alt="${item.izena}" class="card-img-top rounded" />
-              <div class="card-body text-start">
+              <div class="card-body text-center">
                 <h3 class="card-title">${item.izena}</h3>
                 <p class="card-text">${item.deskripzioa}</p>
-                <a class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#productModal-${index}">Eskatu</a>
+                <h4 class="card-price">${item.prezioa}€</h4>
               </div>
             </div>
           </div>
-          <div class="modal fade" id="productModal-${index}" tabindex="-1" role="dialog" aria-labelledby="productModalLabel-${index}" aria-hidden="true">
-            <div class="modal-dialog modal-lg" role="document">
+          
+          <div class="modal fade" id="productModal-${index}" tabindex="-1" aria-labelledby="productModalLabel-${index}" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
               <div class="modal-content">
                 <div class="modal-header">
-                  <h2 class="modal-title">${item.izena}</h2>
-                  <button type="button" class="btn-danger" data-bs-dismiss="modal">X</button>
+                  <h5 class="modal-title">${item.izena}</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body text-start">
+                <div class="modal-body">
                   <div class="row">
-                    <div class="col-md-4">
+                    <div class="col-md-5">
                       <img src="${item.img}" class="img-fluid rounded" alt="${item.izena}" />
                     </div>
                     <div class="col-md-7">
                       <p>${item.deskripzioa}</p>
-                      <div class="my-3">
-                        ${alergenoak}
-                      </div>
-                      <h3 class="my-4">${item.prezioa}€</h3>
-                      <div class="d-flex">
-                        <button class="btn btn-outline-dark btn-sm d-flex decrement" data-index="${index}">-</button>
-                        <span class="mx-3 align-middle d-inline-flex quantity" data-index="${index}" style="height: 30px">1</span>
-                        <button class="btn btn-outline-dark btn-sm d-flex increment" data-index="${index}" data-stock="${item.stock}">+</button>
-                        <img src="${staticUrl}img/cart_negro.png" style="margin-left: 20px; width: 35px; transition: opacity 0.3s ease;" onmouseout="this.src='${staticUrl}img/cart_negro.png'" onmouseover="this.src='${staticUrl}img/cart_rojo.png'" alt="Add to Cart" class="add-to-cart" data-name="${item.izena}" data-price="${item.prezioa}" data-index="${index}" />
+                      <div class="my-3">${alergenoak}</div>
+                      <h4>${item.prezioa}€</h4>
+                      <div class="quantity-controls">
+                        <button class="btn btn-outline-secondary decrement" data-index="${index}">-</button>
+                        <span class="quantity" data-index="${index}">1</span>
+                        <button class="btn btn-outline-secondary increment" data-index="${index}" data-stock="${item.stock}">+</button>
+                        <img src="${staticUrl}img/cart_negro.png" style="width: 35px;" alt="Add to Cart" class="add-to-cart" data-name="${item.izena}" data-price="${item.prezioa}" data-index="${index}" />
                       </div>
                     </div>
                   </div>
@@ -52,24 +64,31 @@ $(document).ready(function () {
         `);
       });
 
+      $(".filter-button").click(function () {
+        const filterValue = $(this).data("filter");
+        if (filterValue === "all") {
+          $(".menu-item").show();
+          $(".category-title").show();
+        } else {
+          $(".menu-item").hide();
+          $(".category-title").hide();
+          $(`.${filterValue}`).show();
+        }
+      });
+
       $(".increment").click(function () {
         const index = $(this).data("index");
         const stock = parseInt($(this).data("stock"));
         let quantityElement = $(`.quantity[data-index="${index}"]`);
         let quantity = parseInt(quantityElement.text());
-
-        if (quantity < stock) {
-          quantityElement.text(quantity + 1);
-        }
+        if (quantity < stock) quantityElement.text(quantity + 1);
       });
 
       $(".decrement").click(function () {
         const index = $(this).data("index");
         let quantityElement = $(`.quantity[data-index="${index}"]`);
         let quantity = parseInt(quantityElement.text());
-        if (quantity > 1) {
-          quantityElement.text(quantity - 1);
-        }
+        if (quantity > 1) quantityElement.text(quantity - 1);
       });
 
       $(".add-to-cart").click(function () {
@@ -78,8 +97,7 @@ $(document).ready(function () {
         const index = $(this).data("index");
         const quantity = parseInt($(`.quantity[data-index="${index}"]`).text());
         addToCart(index + 1, name, price, quantity);
-
-        $(`#productModal-${index}`).modal('hide');
+        $(`#productModal-${index}`).modal("hide");
       });
     },
     error: function (xhr, status, error) {
@@ -88,19 +106,11 @@ $(document).ready(function () {
   });
 
   function addToCart(id, izena, prezioa, quantity) {
-    const cart = JSON.parse(localStorage.getItem('cart')) || [];
-    
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
     const existingProductIndex = cart.findIndex((item) => item.id === id);
-
-    if (existingProductIndex > -1) {
-        cart[existingProductIndex].quantity += quantity;
-    } else {
-        cart.push({ id: id, name: izena, price: prezioa, quantity: quantity });
-    }
-
-    localStorage.setItem('cart', JSON.stringify(cart));
-
-    const notification = $("#notification");
-    notification.stop().fadeIn(300).delay(1000).fadeOut(300);
-}
+    if (existingProductIndex > -1) cart[existingProductIndex].quantity += quantity;
+    else cart.push({ id: id, name: izena, price: prezioa, quantity: quantity });
+    localStorage.setItem("cart", JSON.stringify(cart));
+    $("#notification").stop().fadeIn(300).delay(1000).fadeOut(300);
+  }
 });
